2638024a174a7b51e046d263f37054e1a78673c8
gabrielbressi <gabrielbressi18@gmail.com>
2022-09-19 19:02:21 -0300

    Configured Message Component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="burger-table">
+    <div>
+      <div id="burger-table-heading">
+        <div class="order-id">#:</div>
+        <div>Cliente:</div>
+        <div>Pão:</div>
+        <div>Carne:</div>
+        <div>Opcionais:</div>
+        <div>Ações:</div>
+      </div>
+      <div id="burger-table-rows">
+        <div class="burger-table-row">
+          <div class="order-number">1</div>   
+          <div>João</div>
+          <div>Pão Italiano</div>
+          <div>Alcatra</div>
+          <div>
+            <ul>
+              <li>Salame</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: "Dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
